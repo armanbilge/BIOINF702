ecbaac22d568e18518f38c0874c4a97562a82bcd
Arman Bilge <armanbilge@gmail.com>
2015-05-21 17:55:20 +1200

    Overhaul source code with comments

--- assignment5/example.sh
+++ assignment5/example.sh
@@ -1,1 +1,1 @@
-python3 sim.py -n 20 -t coal -r 1 -l 1000 -f 0.2 0.3 0.2 0.3 -q 1 3 1 1 3 1 -m 0.5 example
+python3 sim.py -n 20 -t coal -b 1 -l 1000 -f 0.2 0.3 0.2 0.3 -q 1 3 1 1 3 1 -m 0.5 example
--- assignment5/sim.py
+++ assignment5/sim.py
@@ -33,70 +33,114 @@ import string
 from Tree import Tree
 from Node import Node
 
+# References:
+#   Felsenstein, Joseph (2004). Inferring Phylogenies. Sinaur Associates.
+#   Gernhard, Tanja (2008). On the conditioned reconstructed process.
+#       Journal of Theoretical Biology 253(4). doi:10.1016/j.jtbi.2008.04.005
+
 def yule_origin(n, lambd):
+    """Sample time of origin for a Yule tree on n taxa with rate lambda."""
+    # Gernhard (2008) Corollary 3.3
     return - np.log(1 - np.power(random.random(), 1/n)) / lambd
 
 def yule_speciations(n, lambd):
-    tor = yule_origin(n, lambd)
-    s = - np.log(1 - (1 - np.exp(-lambd * tor)) * random.random(n-1)) / lambd
-    s.sort()
+    """Sample speciation times for a Yule tree on n taxa with rate lambda."""
+    # Gernhard (2008) Section 2.1.1
+    t = yule_origin(n, lambd)
+    s = - np.log(1 - (1 - np.exp(-lambd * t)) * random.random(n-1)) / lambd
+    s.sort() # Most recent to oldest
     return s
 
 def coal_intervals(n, theta):
+    """Sample coalescent intervals for n taxa from a population of size theta."""
+    # Algorithm given by Felsenstein (2004) Chapter 26 pp. 456
     for k in range(n, 1, -1):
         # Using scale = 1/lambda parameterisation
         yield random.exponential(2 * theta / (k * (k-1)))
 
 def coal_coalescents(n, theta):
+    """Sample coalescent times for n taxa from a population of size theta."""
+    # Running sum of intervals
     return it.accumulate(coal_intervals(n, theta))
 
+# Short names of our tree models
 TREE_MODELS = {'yule': yule_speciations, 'coal': coal_coalescents}
 
 def simulate_tree(n, p, model):
-    nodes = []
+    """Simulate a tree on n taxa according to the model with parameter p."""
+
+    nodes = [] # active nodes
+
+    # Create leaf nodes with integer labels
     for i in map(str, range(n)):
         node = Node(i)
         node.set_height(0)
         nodes.append(node)
+
     for s in model(n, p):
+        # Create parent
         node = Node()
-        i, j = random.choice(nodes, replace=False, size=2)
         node.set_height(s)
-        node.add_child(i)
-        node.add_child(j)
+
+        # Choose children
+        i, j = random.choice(nodes, replace=False, size=2)
         nodes.remove(i)
         nodes.remove(j)
+
+        # Set children
+        node.add_child(i)
+        node.add_child(j)
+
+        # Add to active nodes
         nodes.append(node)
+
+    # Last node is root
     return Tree(nodes[0])
 
 class SubstModel:
+    """Represents a substitution model for molecular evolution along a tree."""
+    # The genetic code
     BASES = ('A', 'C', 'G', 'T')
+    # Standard basis vectors for 4-dimensional space
     E = tuple(np.matrix(np.eye(1, 4, i)).getT() for i in range(4))
 
     def __init__(self, mu, abcdef, pi):
-        self.mu = mu
-        self.pi = pi
+        self.mu = mu # mutation rate
+        self.pi = pi # equilibrium frequencies
         Q = SubstModel.create_Q(abcdef, pi)
+        # Eigenvalue decomposition Q = T L T^-1
+        # Felsenstein (2004) eq. 13.17
         self.L, self.T = LA.eig(Q)
         self.Tinv = LA.inv(self.T)
 
     def transition_matrix(self, t):
+        """Computes the finite time transition matrix."""
+        # Matrix exponential by diagonlisation
+        # Felsenstein (2004) Chapter 13 pp. 206
         return self.T * np.diag(np.exp(t * self.mu * self.L)) * self.Tinv
 
     def random_sequence(self, L):
+        # Draw bases according to equilibrium frequencies
         return ''.join(random.choice(SubstModel.BASES, size=L, p=self.pi))
 
     def mutate_sequence(self, S, t):
+        """Simulates mutations on a sequence for time t according to model."""
         P = self.transition_matrix(t)
+        # Precompute transition probabilities conditioning on ancestral base
         p = {}
-        for e, b in zip(SubstModel.E, SubstModel.BASES):
+        for b, e in zip(SubstModel.BASES, SubstModel.E):
             p[b] = np.squeeze(np.asarray(P * e))
             p[b] /= p[b].sum() # Deal with numerical imprecision
+        # Draw bases according to conditioned transition probabilities
         return ''.join(random.choice(SubstModel.BASES, p=p[x]) for x in S)
 
     def create_Q(abcdef, pi):
+        """Creates the normalised rate matrix for GTR model."""
+
         a, b, c, d, e, f = abcdef
         A, C, G, T = pi
+
+        # Felsenstein (2004) eq. 13.15
         Q = np.matrix([[0.0] * 4 for _ in range(4)])
         Q[0,1] = a * A
         Q[0,2] = b * A
@@ -110,30 +154,39 @@ class SubstModel:
         Q[2,1] = d * G
         Q[3,1] = e * T
         Q[3,2] = f * T
+
+        # Set diagonals so columns sum to 0
         for i in range(4):
             Q[i,i] = - Q[:,i].sum()
+
+        # Felsenstein (2004) eq. 13.14
         beta = 1 / (2 * (a * A * C
                           + b * A * G
                           + c * A * T
                           + d * C * G
                           + e * C * T
                           + f * G * T))
+        # Renormalise Q
         Q *= beta
+
         return Q
 
 def simulate_sequences(node, model, length=None):
-    if length:
+    """Simulates a sequence alignment for a tree under a substition model."""
+    if length: # First call in recursion
         node = node.get_root()
+        # Draw root sequence from equilibrium frequencies
         node.set_sequence(model.random_sequence(length))
-    if not node.is_root():
+    if not node.is_root(): # Simulate mutations during time since parent
         parent = node.get_parent()
         s = model.mutate_sequence(parent.get_sequence(),
                                   parent.get_height() - node.get_height())
         node.set_sequence(s)
-    if not node.is_leaf():
+    if not node.is_leaf(): # Recurse on children
         for child in node.get_children():
             simulate_sequences(child, model)
 
+# Setup command line options
 parser = ArgumentParser(description='Simulate a tree and sequences.')
 parser.add_argument('-n', '--taxa', metavar='N', type=int, required=True,
                     help='the number of taxa')
@@ -142,7 +195,7 @@ parser.add_argument('-t', '--tree-model',
                     type=lambda x: TREE_MODELS[x],
                     choices=TREE_MODELS.values(),
                     required=True, help='the tree model')
-parser.add_argument('-r', '--theta', '--lambda', metavar='R', type=float,
+parser.add_argument('-b', '--theta', '--lambda', metavar='R', type=float,
                     required=True, help='the parameter for the tree model')
 parser.add_argument('-l', '--length', metavar='L', type=int, required=True,
                     help='the length of the sequence')
@@ -159,14 +212,19 @@ parser.add_argument('filename', help='the filename stem')
 
 args = parser.parse_args()
 
+# Simulate the tree
 tree = simulate_tree(args.taxa, args.theta, args.tree_model)
 
+# Write the tree to a file
 with open('{}.tree'.format(args.filename), 'w') as f:
     print(tree.get_newick(), file=f)
 
+# Create the substitution model
 sm = SubstModel(args.mutation_rate, args.rate_matrix, args.frequencies)
+# Simulate the sequence alignment
 simulate_sequences(tree, sm, args.length)
 
+# Write the alignment to a nexus file
 with open('{}.nex'.format(args.filename), 'w') as f:
     print('#NEXUS', file=f)
     print('Begin data;', file=f)
@@ -174,6 +232,7 @@ with open('{}.nex'.format(args.filename), 'w') as f:
           args.length), file=f)
     print('Format datatype=dna missing=? gap=-;', file=f)
     print('Matrix', file=f)
+    # Iterate over leaves in order
     for leaf in sorted(tree.get_leaves(), key=lambda n: int(n.get_label())):
         print('{}    {}'.format(leaf.get_label(), leaf.get_sequence()), file=f)
     print(';', file=f)
